6db93103c1ba4c6d8b1d538350d4703c5364c0c5
Abdalla2004-collab <abdallabakr8072@gmail.com>
2025-05-27 09:57:49 +0100

    correctest eddited version

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from PyQt5.QtWidgets import QWidget, QApplication, QLabel, QPushButton, QLineEdi
 from PyQt5.QtCore import Qt
 from requests import RequestException
 
-#version 3 for the testing as well tbh git hub is useful so far
+#version 1, complete and functional
 #main class
 class WeatherApp(QWidget):
     def __init__(self):
